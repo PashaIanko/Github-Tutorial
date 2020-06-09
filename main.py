d36fe3b139762d29f3ee59f51c56b634cf7d063e
Pavel Ianko <yanpavevg@yandex.ru>
2020-06-09 18:57:55 +0700

    initial local commit

---
+++ main.py
@@ -0,0 +1,10 @@
+#from ApplicationManager import ApplicationManager
+from PyQt5 import QtWidgets
+
+if __name__ == "__main__":
+    import sys
+
+    app = QtWidgets.QApplication(sys.argv)
+#    MainWindow = ApplicationManager()
+#    MainWindow.show()
+    sys.exit(app.exec_())
